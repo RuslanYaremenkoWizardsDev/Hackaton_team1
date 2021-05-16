bd446011b04955405a9886729d0df1f9de6aadb2
DESKTOP-6RDFLBN\Anton <achlp600@gmail.com>
2021-05-16 19:52:44 +0300

    Added participant class

--- TournamentManager/Logic/Participant.cs
+++ TournamentManager/Logic/Participant.cs
@@ -8,51 +8,51 @@ namespace TournamentManager.Logic
 {
     class Participant
     {
-        public static void AddParticipantToDatabase(string participantName)
+        public static void AddParticipantToDatabase(string tournamentName, string participantName)
         {
 
         }
-        public static void AddScoreForParticipant(string participantName, int score)
+        public static void AddScoreForParticipant(string tournamentName, string participantName, int score)
         {
 
         }
-        public static void DeleteScoreForParticipant(string participantName, int stage)
+        public static void DeleteScoreForParticipant(string tournamentName, string participantName, int stage)
         {
 
         }
-        public static void ChangeScoreForParticipant(string participantName, int stage, int score)
+        public static void ChangeScoreForParticipant(string tournamentName, string participantName, int stage, int score)
         {
 
         }
-        public static void ClearScoreForParticipant(string participantName)
+        public static void ClearScoreForParticipant(string tournamentName, string participantName)
         {
 
         }
-        public static void DeleteParticipant(string participantName)
+        public static void DeleteParticipant(string tournamentName, string participantName)
         {
 
         }
-        public static int GetScoreForParticipant(string participantName, int stage)
+        public static int GetScoreForParticipant(string tournamentName, string participantName, int stage)
         {
             return 0;
         }
-        public static int[] GetScoreForParticipant(string participantName)
+        public static int[] GetScoreForParticipant(string tournamentName, string participantName)
         {
             return new int[] { 0 };
         }
-        public static void SetOpponentForParticipant(string participantName, string opponentName)
+        public static void SetOpponentForParticipant(string tournamentName, string participantName, string opponentName)
         {
 
         }
-        public static string GetOpponentNameOfParticipant(string participantName)
+        public static string GetOpponentNameOfParticipant(string tournamentName, string participantName)
         {
             return "";
         }
-        public static int GetOpponentScoreOfParticipant(string participantName, int stage)
+        public static int GetOpponentScoreOfParticipant(string tournamentName, string participantName, int stage)
         {
             return 0;
         }
-        public static int[] GetOpponentScoreOfParticipant(string participantName)
+        public static int[] GetOpponentScoreOfParticipant(string tournamentName, string participantName)
         {
             return new int[] { 0 };
         }
