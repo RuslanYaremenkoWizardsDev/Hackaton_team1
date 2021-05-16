b7ecf76f62d10f9ed14f3bb49885ba680f0a3ae7
DESKTOP-6RDFLBN\Anton <achlp600@gmail.com>
2021-05-16 16:01:45 +0300

    Added empty methods to class Participant

--- TournamentManager/Logic/Participant.cs
+++ TournamentManager/Logic/Participant.cs
@@ -8,5 +8,53 @@ namespace TournamentManager.Logic
 {
     class Participant
     {
+        public static void AddParticipantToDatabase(string participantName)
+        {
+
+        }
+        public static void AddScoreForParticipant(string participantName, int score)
+        {
+
+        }
+        public static void DeleteScoreForParticipant(string participantName, int stage)
+        {
+
+        }
+        public static void ChangeScoreForParticipant(string participantName, int stage, int score)
+        {
+
+        }
+        public static void ClearScoreForParticipant(string participantName)
+        {
+
+        }
+        public static void DeleteParticipant(string participantName)
+        {
+
+        }
+        public static int GetScoreForParticipant(string participantName, int stage)
+        {
+            return 0;
+        }
+        public static int[] GetScoreForParticipant(string participantName)
+        {
+            return new int[] { 0 };
+        }
+        public static void SetOpponentForParticipant(string participantName, string opponentName)
+        {
+
+        }
+        public static string GetOpponentNameOfParticipant(string participantName)
+        {
+            return "";
+        }
+        public static int GetOpponentScoreOfParticipant(string participantName, int stage)
+        {
+            return 0;
+        }
+        public static int[] GetOpponentScoreOfParticipant(string participantName)
+        {
+            return new int[] { 0 };
+        }
     }
 }
