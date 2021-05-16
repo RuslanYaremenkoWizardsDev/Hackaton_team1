a46722f9e11e51127ce94bbb6adf0a802272ae9b
DESKTOP-6RDFLBN\Anton <achlp600@gmail.com>
2021-05-16 09:29:33 +0300

    fix Random data generation class

--- TournamentManagerUnitTests/RandomDataGenerationUnitTest.cs
+++ TournamentManagerUnitTests/RandomDataGenerationUnitTest.cs
@@ -13,21 +13,27 @@ namespace TournamentManagerUnitTests
         [Test]
         public void GenerateFullMaleNameTest()
         {
-            string fullMaleName = RandomDataGeneration.GenerateFullMaleName();
-            string[] fullMaleNameWords = fullMaleName.Split(' ');
-            Assert.AreEqual(3, fullMaleNameWords.Length);
-            Assert.AreNotEqual(' ', fullMaleNameWords[0]);
-            Assert.AreNotEqual(' ', fullMaleNameWords[^1]);
+            for (int i = 0; i < 1000; i++)
+            {
+                string fullMaleName = RandomDataGeneration.GenerateFullMaleName();
+                string[] fullMaleNameWords = fullMaleName.Split(' ');
+                Assert.AreEqual(3, fullMaleNameWords.Length);
+                Assert.AreNotEqual(' ', fullMaleNameWords[0]);
+                Assert.AreNotEqual(' ', fullMaleNameWords[^1]);
+            }
         }
 
         [Test]
         public void GenerateFullFemaleNameTest()
         {
-            string fullFemaleName = RandomDataGeneration.GenerateFullFemaleName();
-            string[] fullFemaleNameWords = fullFemaleName.Split(' ');
-            Assert.AreEqual(3, fullFemaleNameWords.Length);
-            Assert.AreNotEqual(' ', fullFemaleNameWords[0]);
-            Assert.AreNotEqual(' ', fullFemaleNameWords[^1]);
+            for (int i = 0; i < 1000; i++)
+            {
+                string fullFemaleName = RandomDataGeneration.GenerateFullFemaleName();
+                string[] fullFemaleNameWords = fullFemaleName.Split(' ');
+                Assert.AreEqual(3, fullFemaleNameWords.Length);
+                Assert.AreNotEqual(' ', fullFemaleNameWords[0]);
+                Assert.AreNotEqual(' ', fullFemaleNameWords[^1]);
+            }
         }
     }
 }
